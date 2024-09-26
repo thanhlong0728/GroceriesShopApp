c80baef11459c408d3cc8010bae0b3e65ae97867
thanhlong0728 <thanhlong0728@gmail.com>
2024-09-26 11:26:32 +0700

    Initial Commit

---
+++ GroceriesShopApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GroceriesShopApp
+//
+//  Created by Mac on 26/09/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GroceriesShopApp/GroceriesShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceriesShopAppApp.swift
+//  GroceriesShopApp
+//
+//  Created by Mac on 26/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct GroceriesShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
