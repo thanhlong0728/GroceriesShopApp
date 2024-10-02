a08c05439821edf15786d784c4063867654494e3
thanhlong0728 <thanhlong0728@gmail.com>
2024-10-02 15:49:57 +0700

    tabbar navigation view

---
+++ GroceriesShopApp/View/Explore/ExploreView.swift
@@ -0,0 +1,18 @@
+//
+//  ExploreView.swift
+//  GroceriesShopApp
+//
+//  Created by Mac on 02/10/2024.
+//
+
+import SwiftUI
+
+struct ExploreView: View {
+    var body: some View {
+        Text("Hello, World! Exxxxx")
+    }
+}
+
+#Preview {
+    ExploreView()
+}
--- GroceriesShopApp/View/MainTabView/MainTabView.swift
+++ GroceriesShopApp/View/MainTabView/MainTabView.swift
@@ -8,14 +8,59 @@
 import SwiftUI
 
 struct MainTabView: View {
+    
+    @StateObject var homeVM = HomeViewModel.shared
+    
     var body: some View {
         ZStack{
-            HStack{
-                Button{
-                    MainViewModel.shared.isUserLogin = false
-                }label: {
-                    Text("Logout")
+            VStack{
+                Spacer()
+                HStack{
+                    TabButton(title: "Shop", icon: "store_tab", isSelect: homeVM.selectTab == 0) {
+                        print("Button Tab")
+                        DispatchQueue.main.async {
+                            withAnimation {
+                                homeVM.selectTab = 0
+                            }
+                        }
+                    }
+                    TabButton(title: "Explore", icon: "explore_tab", isSelect: homeVM.selectTab == 1) {
+                        DispatchQueue.main.async {
+                            withAnimation {
+                                homeVM.selectTab = 1
+                            }
+                        }
+                    }
+                    TabButton(title: "Cart", icon: "cart_tab", isSelect: homeVM.selectTab == 2) {
+                        DispatchQueue.main.async {
+                            withAnimation {
+                                homeVM.selectTab = 2
+                            }
+                        }
+                    }
+                    
+                    TabButton(title: "Favourite", icon: "fav_tab", isSelect: homeVM.selectTab == 3) {
+                        DispatchQueue.main.async {
+                            withAnimation {
+                                homeVM.selectTab = 3
+                            }
+                        }
+                    }
+                    
+                    TabButton(title: "Account", icon: "account_tab", isSelect: homeVM.selectTab == 4) {
+                        DispatchQueue.main.async {
+                            withAnimation {
+                                homeVM.selectTab = 4
+                            }
+                        }
+                    }
                 }
+                .padding(.top, 10)
+                .padding(.bottom, .bottomInsets)
+                .padding(.horizontal, 10)
+                .background(Color.white)
+                .cornerRadius(15)
+                .shadow(color: Color.black.opacity(0.15), radius: 3, x: 0, y: -2)
             }
         }
         .navigationTitle("")
@@ -24,6 +69,7 @@ struct MainTabView: View {
         .ignoresSafeArea()
     }
 }
+
 
 #Preview {
     MainTabView()
