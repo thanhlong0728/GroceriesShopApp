3d23d015c627b401195d9d368d8ad01e9967d6de
thanhlong0728 <thanhlong0728@gmail.com>
2024-10-02 15:06:06 +0700

    navigation link main tab

---
+++ GroceriesShopApp/View/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  GroceriesShopApp
+//
+//  Created by Mac on 01/10/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ GroceriesShopApp/ViewModel/HomeViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewModel.swift
+//  GroceriesShopApp
+//
+//  Created by Mac on 01/10/2024.
+//
+
+import SwiftUI
+
+struct HomeViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeViewModel()
+}
