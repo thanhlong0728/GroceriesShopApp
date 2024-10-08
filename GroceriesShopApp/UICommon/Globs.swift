bfd80ba88a09bd7b4761f36819d232dc04bf210f
thanhlong0728 <thanhlong0728@gmail.com>
2024-10-08 16:48:06 +0700

    favourite list screen

--- GroceriesShopApp/UICommon/Globs.swift
+++ GroceriesShopApp/UICommon/Globs.swift
@@ -21,6 +21,8 @@ struct Globs {
     static let SV_SIGN_UP = BASE_URL + "sign_up"
     static let SV_HOME = BASE_URL + "home"
     static let SV_PRODUCT_DETAIL = BASE_URL + "product_detail"
+    static let SV_ADD_REMOVE_FAVORITE = BASE_URL + "add_remove_favorite"
+    static let SV_FAVORITE_LIST = BASE_URL + "favorite_list"
 }
 
 struct KKey {
